ccccda7ea5d7c00fdb287989435d1b7e54810c35
TikuGaelle <brendagaelle6@gmail.com>
2021-10-24 22:26:02 -0700

    Updated main.dart file containing row widget which displays two dropdown menus and a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dropdown.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+String dropdownvalue = 'Apple'; String newValue = " ";
+  var items =  ['Apple','Banana','Grapes','Orange','watermelon','Pineapple'];
+
+
+DropdownButton<String> buildDropdownButton() {
+    return DropdownButton(
+              value: dropdownvalue,
+                icon: Icon(Icons.keyboard_arrow_down),
+                items:items.map((String items) {
+                     return DropdownMenuItem(
+                         value: items,
+                         child: Text(items)
+                     );
+                }
+                ).toList(),
+              onChanged: (newValue){
+                setState(() {
+                   newValue = dropdownvalue;
+                });
+              }
+            );
+}
+
+ 
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -33,8 +58,28 @@ class _MyHomePageState extends State<MyHomePage> {
          title: Text("Currency Converter App"),
       ),
       body: ListView(
+        children: [
 
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildDropdownButton(),
+                ElevatedButton.icon(
+            icon: Icon(Icons.arrow_forward), 
+            label: Text(" "),
+              onPressed: () {
+   //some function
+               },),
+                buildDropdownButton()
+              ]
+            ),
+          )
+        ],
       ),
    );
   }
 }
+  
