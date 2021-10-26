f4ebd1892095ed8c297da10bded69ff778887dee
TikuGaelle <brendagaelle6@gmail.com>
2021-10-26 01:47:14 -0700

    Updated main.dart file containing textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'getData.dart';
 
@@ -50,8 +49,7 @@ DropdownButton<String> buildDropdownButton() {
               }
             );
 }
- void getData() async{
-   var tmp = await CurrencyList().getData();
+ void getData() async{   var tmp = await CurrencyList().getData();
   
  }
  
@@ -82,105 +80,21 @@ DropdownButton<String> buildDropdownButton() {
                   ],
                ),
              ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                buildDropdownButton(),
-                ElevatedButton.icon(
-            icon: Icon(Icons.arrow_forward), 
-            label: Text(" "),
-              onPressed: () {
-   //some function
-               },),
-                buildDropdownButton()
-              ]
-            ),
-          )
-     ] ));
-  }
-}
-  
-=======
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(   
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-String dropdownvalue = 'Apple'; String newValue = " ";
-  var items =  ['Apple','Banana','Grapes','Orange','watermelon','Pineapple'];
-
-
-DropdownButton<String> buildDropdownButton() {
-    return DropdownButton(
-              value: dropdownvalue,
-                icon: Icon(Icons.keyboard_arrow_down),
-                items:items.map((String items) {
-                     return DropdownMenuItem(
-                         value: items,
-                         child: Text(items)
-                     );
-                }
-                ).toList(),
-              onChanged: (newValue){
-                setState(() {
-                   newValue = dropdownvalue;
-                });
-              }
-            );
-}
-
- 
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: AppBar(
-         title: Text("Currency Converter App"),
-      ),
-      body: ListView(
-          children: [
-             // adding the texts for issue #3
-             Center(
-               child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text('Exchange Rate', style: TextStyle(
-                      color: Colors.black38 ),),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text('CFA 566.22', style: TextStyle(
-                      color: Colors.black87, fontWeight: FontWeight.bold,
-                    ),),
-                  ],
-               ),
+             Padding(
+               padding: const EdgeInsets.all(20.0),
+               child: TextField(             
+                    decoration: InputDecoration(
+                //contentPadding: EdgeInsets.only(top: 3),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+            labelText: 'Answer',
+                  ),
+                  ),
+             ),
+             SizedBox(
+               height: 20,
              ),
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -202,5 +116,4 @@ DropdownButton<String> buildDropdownButton() {
      ] ));
   }
 }
-  
->>>>>>> 5e7928fd3130eb5d86c6b39828077196a3d6081a
+
